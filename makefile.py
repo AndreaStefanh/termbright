d034da2c667fd796541aacaa3b4a339d990b2741
Andrea Stefan <andreastefan0101@gmail.com>
2022-01-24 20:17:29 +0100

    log commands like makefile

--- makefile.py
+++ makefile.py
@@ -32,6 +32,7 @@ DIR = [
 def createDir() -> None:
 
     for i in range(0, len(DIR)):
+        print(f"mkdir -p {DIR[i]}")
         os.makedirs(DIR[i], exist_ok=True)
     
     return
@@ -39,6 +40,7 @@ def createDir() -> None:
 def src2obj() -> None:
 
     for i in range(0, len(SRC)):
+        print(f"{CC} {CFLAGS} {SRC[i]} -o {OBJ[i]}")
         os.system(f"{CC} {CFLAGS} {SRC[i]} -o {OBJ[i]}")
     
     return
@@ -46,6 +48,7 @@ def src2obj() -> None:
 def obj2bin() -> None:
 
     obj = " ".join(OBJ)
+    print(f"{CC} {obj} -o bin/termbright")
     os.system(f"{CC} {obj} -o bin/termbright")
     return
 
@@ -61,16 +64,19 @@ def args() -> None:
         for i in range(2, len(sys.argv)):
             buffer = buffer + sys.argv[i]
         
+        print(buffer + "\n------------------")
         os.system(buffer)
     
     elif sys.argv[1] == "install":
 
         if os.getuid() != 0:
-            print("For this operation need root permition")
+            print("[!] For this operation need root permition")
             exit(1)
         
+        print("sudo cp bin/termbright", "/usr/bin/termbright")
         shutil.copyfile("bin/termbright", "/usr/bin/termbright")
 
+        print("sudo chmod 555 /usr/bin/termbright")
         os.chmod(
             "/usr/bin/termbright",
             stat.S_IXUSR |
@@ -78,9 +84,7 @@ def args() -> None:
             stat.S_IXOTH |
             stat.S_IRUSR |
             stat.S_IRGRP |
-            stat.S_IROTH |
-            stat.S_IWUSR |
-            stat.S_IWGRP
+            stat.S_IROTH
         )
 
 
